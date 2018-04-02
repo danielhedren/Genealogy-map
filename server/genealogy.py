b0a34a401c9b2cacd7de4976c720c3974ea2c1a6
Daniel Hedrén <danielhedren@gmail.com>
2018-04-02 20:20:14 +0200

    Switched to sqlite3

--- server/genealogy.py
+++ server/genealogy.py
@@ -1,54 +1,67 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_restful import Resource, Api
-from sqlalchemy import create_engine
+#from sqlalchemy import create_engine
+import sqlite3
 from json import dumps
 import urllib
 import json
 import config
+import queue
 
-e = create_engine('sqlite:///database.s3db')
+#e = create_engine('sqlite:///database.s3db')
+db = sqlite3.connect("database.s3db")
 
 app = Flask(__name__)
 api = Api(app)
+
+geocode_queue = queue.Queue()
+
+def process_queue():
+    while True:
+        address = geocode_queue.get()
+        if address is None:
+            break
 
 class GeocodePost(Resource):
     def get(self):
         return ""
     def post(self):
         json_data = request.get_json(force=True)
-        conn = e.connect()
+        cur = db.cursor()
 
         locations_list = []
 
         for i, address in enumerate(json_data):
-            print(str(i) + "/" + str(len(json_data)))
-            result = conn.execute("select count(*) from geocodes where address=?", address).first()
+            result = cur.execute("select count(*) from geocodes where address=?", (address,)).fetchone()
             if (result[0] == 0):
-                response = urllib.request.urlopen("https://maps.googleapis.com/maps/api/geocode/json?address=" + urllib.parse.quote_plus(address) + "&key=" + config.gmaps_api_key)
-                content = response.read().decode(response.headers.get_content_charset())
-                json_data = json.loads(content)
-                if (json_data["status"] == "OK"):
-                    values = [address,json_data["results"][0]["geometry"]["location"]["lat"], json_data["results"][0]["geometry"]["location"]["lng"], True, "google"]
-                    conn.execute("insert into geocodes (address, latitude, longitude, valid, source) values (?, ?, ?, ?, ?)", values)
-                elif (json_data["status"] == "ZERO_RESULTS"):
-                    values = [address, False, "google"]
-                    conn.execute("insert into geocodes (address, valid, source) values (?, ?, ?)", values)
-                else:
-                    print("STATUS ERROR " + json_data["status"])
-                    break
+                print("Adding address " + address + " to queue.")
+                geocode_queue.put(address)
+                # response = urllib.request.urlopen("https://maps.googleapis.com/maps/api/geocode/json?address=" + urllib.parse.quote_plus(address) + "&key=" + config.gmaps_api_key)
+                # content = response.read().decode(response.headers.get_content_charset())
+                # json_data = json.loads(content)
+                # if (json_data["status"] == "OK"):
+                #     values = [address,json_data["results"][0]["geometry"]["location"]["lat"], json_data["results"][0]["geometry"]["location"]["lng"], True, "google"]
+                #     conn.execute("insert into geocodes (address, latitude, longitude, valid, source) values (?, ?, ?, ?, ?)", values)
+                # elif (json_data["status"] == "ZERO_RESULTS"):
+                #     values = [address, False, "google"]
+                #     conn.execute("insert into geocodes (address, valid, source) values (?, ?, ?)", values)
+                # else:
+                #     print("STATUS ERROR " + json_data["status"])
+                #     break
             else:
-                location = conn.execute("select latitude, longitude from geocodes where address=? and valid", address).first()
+                location = cur.execute("select latitude, longitude from geocodes where address=? and valid", (address,)).fetchone()
                 if (location != None):
                     locations_list.append({"address":address, "latitude":location[0], "longitude":location[1]})
         
+        print("Queue size is now approx " + str(geocode_queue.qsize()))
         return jsonify(locations_list)
 
 class Geocode(Resource):
     def get(self, address):
         print(address)
-        conn = e.connect()
-        query = conn.execute("select * from geocodes where address='%s'" % address)
+        cur = db.cursor()
+        query = cur.execute("select * from geocodes where address=?", (address,))
         # Query the result and get cursor.Dumping that data to a JSON is looked by extension
         result = {'data': [dict(zip(tuple(query.keys()), i)) for i in query.cursor]}
         return result
