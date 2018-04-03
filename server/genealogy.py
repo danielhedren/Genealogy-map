4a8ccf8b01ac532629b04798353ef3904ef71371
Daniel Hedrén <danielhedren@gmail.com>
2018-04-03 22:14:25 +0200

    Concurrency woes

--- server/genealogy.py
+++ server/genealogy.py
@@ -9,8 +9,12 @@ import config
 import queue
 import threading
 import time
+import logging
 
-db = sqlite3.connect("database.s3db")
+logging.basicConfig(filename="genealogy.log", level=logging.DEBUG)
+logging.debug("Log started")
+
+db = sqlite3.connect("database.s3db", check_same_thread=False)
 
 app = Flask(__name__)
 api = Api(app)
@@ -22,27 +26,39 @@ queueProcessLock = threading.Lock()
 geocode_result_queue = queue.Queue()
 
 def process_queue():
+    logging.debug("Queue processing worker thread started")
     db = sqlite3.connect("database.s3db")
-    while not queueProcessEvent.is_set():
+    while not queueProcessEvent.is_set() and not geocode_queue.empty():
+        logging.debug("Queue processing worker waiting for work")
         address = geocode_queue.get()
+        logging.debug("Queue processing worker work started")
         
         try:
             response = urllib.request.urlopen("https://maps.googleapis.com/maps/api/geocode/json?address=" + urllib.parse.quote_plus(address) + "&key=" + config.gmaps_api_key)
+            logging.debug("Response sent")
         except urllib.error.HTTPError:
             geocode_queue.task_done()
             geocode_result_queue.put({"json_data": {"status": "INVALID_REQUEST"}, "address": address})
             continue
+        
+        logging.debug("Response no error")
 
         content = response.read().decode(response.headers.get_content_charset())
         json_data = json.loads(content)
 
         geocode_result_queue.put({"json_data": json_data, "address": address})
 
+        logging.debug("Queue processing worker task done")
         geocode_queue.task_done()
 
+    logging.debug("Queue processing worker finished")
+
 def process_queue_results():
+    logging.debug("Queue processing worker thread started")
     db = sqlite3.connect("database.s3db")
-    while not queueProcessEvent.is_set():
+    while not queueProcessEvent.is_set() and not (geocode_result_queue.empty() and geocode_queue.empty()):
+        logging.debug("Queue result worker waiting for work")
+
         data = geocode_result_queue.get()
         address = data["address"]
         json_data = data["json_data"]
@@ -59,7 +75,8 @@ def process_queue_results():
             db.commit()
         elif (json_data["status"] == "UNKNOWN_ERROR"):
             # Retry on unknown error
-            geocode_queue.put(address)
+            # geocode_queue.put(address)
+            pass
         elif (json_data["status"] == "OVER_QUERY_LIMIT"):
             # Kill the threads
             queueProcessEvent.set()
@@ -70,12 +87,13 @@ def process_queue_results():
         global geocode_queue_done
         geocode_queue_done += 1
 
+        logging.debug("Queue result worker task done")
         geocode_result_queue.task_done()
 
-for i in range(0, 1):
-    threading.Thread(target=process_queue, daemon=True).start()
+    logging.debug("Queue result worker finished")
 
-threading.Thread(target=process_queue_results, daemon=True).start()
+process_queue_workers = []
+process_result_worker = None
 
 class GeocodePost(Resource):
     def get(self):
@@ -90,7 +108,7 @@ class GeocodePost(Resource):
 
         for i, address in enumerate(json_data):
             result = cur.execute("select count(*) from geocodes where address=?", (address,)).fetchone()
-            if (result[0] == 0):
+            if (result[0] == 0 and address != ""):
                 geocode_queue.put(address)
                 queue_added += 1
             else:
@@ -98,12 +116,20 @@ class GeocodePost(Resource):
                 if (location != None):
                     locations_list.append({"address":address, "latitude":location[0], "longitude":location[1]})
         
-        print("Queue size is now approx " + str(geocode_queue.qsize()))
+        logging.debug("Queue size is now approx " + str(geocode_queue.qsize()))
+
+        if geocode_queue.qsize() > 0:
+            for i in range(0, 1):
+                process_queue_workers.append(threading.Thread(target=process_queue, daemon=False))
+                process_queue_workers[len(process_queue_workers) - 1].start()
+
+            process_result_worker = threading.Thread(target=process_queue_results, daemon=False)
+            process_result_worker.start()
+
         return jsonify({"queue_done": geocode_queue_done, "queue_target": geocode_queue_done + queue_added, "data": locations_list})
 
 class Geocode(Resource):
     def get(self, address):
-        print(address)
         cur = db.cursor()
         query = cur.execute("select * from geocodes where address=?", (address,))
         # Query the result and get cursor.Dumping that data to a JSON is looked by extension
