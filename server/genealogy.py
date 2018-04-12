50d9378f6182ae14acc97d34c4a1019c745bc52c
Daniel Hedrén <danielhedren@gmail.com>
2018-04-12 16:50:07 +0200

    Removed CORS

--- server/genealogy.py
+++ server/genealogy.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
 from flask_restful import Resource, Api
 from json import dumps
 import json
@@ -7,15 +6,15 @@ import config
 import logging
 import psycopg2
 from psycopg2.extras import execute_batch
+import time
 
-logging.basicConfig(level=logging.CRITICAL)
-logging.debug("Log started")
+logging.basicConfig(level=logging.DEBUG)
 
 db = psycopg2.connect(config.postgres_connection_string)
 
 with db.cursor() as cur:
     cur.execute("CREATE TABLE IF NOT EXISTS geocodes (address TEXT, latitude DOUBLE PRECISION, longitude DOUBLE PRECISION, valid BOOLEAN, source VARCHAR(50), PRIMARY KEY (address, source));")
-    cur.execute("CREATE INDEX IF NOT EXISTS geocodes_la_idx ON geocodes (lower(address));")
+    cur.execute("CREATE INDEX IF NOT EXISTS geocodes_la_idx ON geocodes (address);")
     cur.execute("CREATE TABLE IF NOT EXISTS geocodes_pending (id BIGSERIAL PRIMARY KEY, address TEXT UNIQUE, status SMALLINT DEFAULT 0);")
     db.commit()
 
@@ -26,29 +25,31 @@ class GeocodePost(Resource):
     def get(self):
         return ""
     def post(self):
-        logging.debug("Post entered")
-        json_data = request.get_json(force=True)
+        t0 = time.time()
+        json_data = set(request.get_json(force=True))
+        logging.debug("Json conversion took " + str(time.time() - t0) + " s")
+
         with db.cursor() as cur:
             locations_list = []
-            data_set = set(json_data)
-            logging.debug(tuple(data_set))
 
+            t0 = time.time()
             cur.execute("SELECT address, latitude, longitude, valid FROM geocodes WHERE address in %s ORDER BY valid DESC;", (tuple(json_data),))
+            logging.debug("Select took " + str(time.time() - t0) + " s")
+
+            t0 = time.time()
             for result in cur:
-                logging.debug(result[0])
                 try:
-                    data_set.remove(result[0])
+                    json_data.remove(result[0])
                 except KeyError:
                     pass
                 else:
                     if result[3] == True:
                         locations_list.append({"address":result[0], "latitude":result[1], "longitude":result[2]})
-            
-            logging.debug(data_set)
-            logging.debug("Inserting into pending")
+            logging.debug("Result loop took " + str(time.time() - t0) + " s")
 
+            t0 = time.time()
             try:
-                execute_batch(cur, "INSERT INTO geocodes_pending (address) VALUES (%s) ON CONFLICT DO NOTHING;", [[a] for a in data_set])
+                execute_batch(cur, "INSERT INTO geocodes_pending (address) VALUES (%s) ON CONFLICT DO NOTHING;", [[a] for a in json_data])
             except psycopg2.DataError as e:
                 logging.debug(e)
             else:
@@ -65,7 +66,7 @@ class GeocodePost(Resource):
             else:
                 queue_current = 0
 
-            logging.debug("Queue current " + str(queue_current) + " queue target " + str(queue_target))
+            logging.debug("Rest took " + str(time.time() - t0) + " s")
 
             return jsonify({"queue_current": queue_current, "queue_target": queue_target, "data": locations_list})
 
@@ -89,7 +90,6 @@ class QueueStatus(Resource):
 class GeocodeInsert(Resource):
     def post(self):
         data = request.get_json(force=True)
-        logging.debug(data)
         
         with db.cursor() as cur:
             logging.debug("Inserting")
